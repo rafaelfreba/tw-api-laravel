653f4f823c4acc9114021fe119123b841e8ae0dd
Rafael Batista <rafaelfreba@gmail.com>
2022-12-13 14:11:14 -0400

    Refatorando o tratamento de erros

--- app/Exceptions/ApiHandler.php
+++ app/Exceptions/ApiHandler.php
@@ -4,32 +4,77 @@ namespace App\Exceptions;
 
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Response;
 use Throwable;
 
 trait ApiHandler
 {
-    public function tratarErros(Throwable $exception)
+    /**
+     * Trata os erros personalizados
+     *
+     * @param Throwable $exception
+     * @return Response
+     */
+    public function tratarErros(Throwable $exception): Response
     {
         //se ocorrer uma exceção do tipo específico
         if ($exception instanceof ModelNotFoundException) {
-            return $this->repostaPadrao(
-                'registro não encontrado',
-                'o sistema não encontrou o registro que você está buscando',
-                404
-            );
+
+            return $this->modelNotFoundException();
         }
+
         //personalizando erros de validação
         if ($exception instanceof ValidationException) {
-            return $this->repostaPadrao(
-                'erro de validação',
-                'os dados enviados são inválidos',
-                404,
-                $exception->errors()
-            );
+
+            return $this->validationException($exception);
         }
     }
 
-    public function repostaPadrao(string $code, string $message, int $status, array $errors = null)
+    /**
+     * Retorna o erro quando não encontrado o registro
+     *
+     * @return Response
+     */
+    public function modelNotFoundException(): Response
+    {
+        return $this->repostaPadrao(
+            'registro não encontrado',
+            'o sistema não encontrou o registro que você está buscando',
+            404
+        );
+    }
+
+    /**
+     * Retorna o erro quando os dados são inválidos
+     *
+     * @param ValidationException $e
+     * @return Response
+     */
+    public function validationException(ValidationException $e): Response
+    {
+        return $this->repostaPadrao(
+            'erro de validação',
+            'os dados enviados são inválidos',
+            404,
+            $e->errors()
+        );
+    }
+
+    /**
+     * Retorna uma resposta padrão para os erros da API
+     *
+     * @param string $code
+     * @param string $message
+     * @param integer $status
+     * @param array|null $errors
+     * @return Response
+     */
+    public function repostaPadrao(
+            string $code, 
+            string $message, 
+            int $status, 
+            array $errors = null
+        ): Response
     {
         $dadosResposta = [
             'code' => $code,
